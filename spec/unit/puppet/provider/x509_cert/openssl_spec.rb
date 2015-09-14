692f1443fd0a482ec989716a25ddd384bf52b0b2
Denis Egorenko <degorenko@mirantis.com>
2015-09-14 19:18:11 +0300

    Change cert existance logic
    Add checking commonName and subjectAltName in old certificate
    if it exists. In other way we could not regenerate a new certificate
    if any of these fields change.
    
    Closes-Bug: #1490966
    
    Upstream pull-request: https://github.com/camptocamp/puppet-openssl/pull/51
    Change-Id: I1375a9d1ce5f50e84edee4ecc21614450408ae73

--- spec/unit/puppet/provider/x509_cert/openssl_spec.rb
+++ spec/unit/puppet/provider/x509_cert/openssl_spec.rb
@@ -12,6 +12,10 @@ describe 'The openssl provider for the x509_cert type' do
   context 'when not forcing key' do
     it 'exists? should return true if certificate exists and is synced' do
       Pathname.any_instance.expects(:exist?).returns(true)
+      File.stubs(:read).returns('')
+      c = OpenSSL::X509::Certificate.new
+      OpenSSL::X509::Certificate.stubs(:new).returns(c)
+      IniFile.stubs(:load).returns([])
       expect(subject.exists?).to eq(true)
     end
 
@@ -51,6 +55,7 @@ describe 'The openssl provider for the x509_cert type' do
     it 'exists? should return true if certificate exists and is synced' do
       resource[:force] = true
       File.stubs(:read)
+      IniFile.stubs(:load).returns([])
       Pathname.any_instance.expects(:exist?).returns(true)
       c = OpenSSL::X509::Certificate.new # Fake certificate for mocking
       OpenSSL::X509::Certificate.stubs(:new).returns(c)
